1fb44925a6613e7527d9312131dfd363083d2389
Andrey Kukuruza <andreyku6@gmail.com>
2018-08-30 12:22:16 +0300

    add scroll to top buttom

---
+++ js/scrollToTop.js
@@ -0,0 +1,22 @@
+$('#scroll-to-top').on('click', function (e) {
+    //e.preventDefault();
+    console.log(1);
+    $('html,body').animate({
+        scrollTop: 0
+    }, 700);
+});
+
+const scrollTriggerFromBottom = 1800;// px
+const toogleButton = function () {
+    const scrollTop = $(window).scrollTop();
+    console.log(scrollTop);
+    console.log($(document).height() - scrollTriggerFromBottom);
+    if (scrollTop > $(document).height() - scrollTriggerFromBottom) {
+        $('#scroll-to-top').removeClass('hide');
+    } else {
+        $('#scroll-to-top').addClass('hide');
+    }
+};
+$(window).on('scroll', function () {
+    toogleButton();
+});
