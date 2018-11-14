14a790250910f31524794f677c19ac4521eedeae
DenysVolynets <39590454+DenysVolynets@users.noreply.github.com>
2018-11-14 18:59:47 +0200

    Add map-panel for skylar

---
+++ js/skylar-map.js
@@ -0,0 +1,39 @@
+L.mapbox.accessToken = 'pk.eyJ1IjoibWVyaWRpYW5pbnRlcm5hdGlvbmFsIiwiYSI6ImNqb2ZwZ3pyMDA0dW4za213eHdsdWYzZmYifQ.SHgtcRk7sZemuADSZQY3Vg';
+var mymap = L.map('map').setView([11.54, 104.92], 14);
+
+L.tileLayer(
+    'https://api.mapbox.com/styles/v1/meridianinternational/cjog2mfuv2pyo2st95cw238xp/tiles/{z}/{x}/{y}?access_token=' + L.mapbox.accessToken, {
+        tileSize: 512,
+        zoomOffset: -1,
+        maxZoom: 16,
+        minZoom: 12,
+        maxBoundsViscosity: 0.9,
+        bounceAtZoomLimits: false,
+        tms: false,
+        noWrap: true,
+        opacity: 1.0,
+    }).addTo(mymap);
+
+mymap.setMaxBounds([
+    [11.475132, 104.8365973],
+    [11.622882, 105.020376]
+]);
+
+const markerData = {
+    skylar: {
+        icon: {
+            className: 'custom-marker-left-up skylar',
+            html: '<h5>SKYLAR BY MERIDIAN</h5>',
+            title: "skylar"
+        },
+        position: [11.5358442, 104.926587]
+    }
+};
+
+for (const markerDataKey in markerData) {
+    const marker = L.marker(markerData[markerDataKey].position, {
+        icon: L.divIcon(markerData[markerDataKey].icon),
+        zIndexOffset: 10000
+    }).addTo(mymap);
+}
+
