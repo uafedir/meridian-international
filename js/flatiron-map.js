4d56a15baa93dedd64c215ab765c9cec92d4ae88
DenysVolynets <39590454+DenysVolynets@users.noreply.github.com>
2018-11-21 19:23:07 +0200

    Flatiron-map added

---
+++ js/flatiron-map.js
@@ -0,0 +1,41 @@
+L.mapbox.accessToken = 'pk.eyJ1IjoibWVyaWRpYW5pbnRlcm5hdGlvbmFsIiwiYSI6ImNqb2ZwZ3pyMDA0dW4za213eHdsdWYzZmYifQ.SHgtcRk7sZemuADSZQY3Vg';
+var mymap = L.map('map').setView([11.577, 104.91], 14);
+
+L.tileLayer(
+    'https://api.mapbox.com/styles/v1/meridianinternational/cjog2mfuv2pyo2st95cw238xp/tiles/{z}/{x}/{y}?access_token=' + L.mapbox.accessToken, {
+        tileSize: 512,
+        zoomOffset: -1,
+        maxZoom: 16,
+        minZoom: 12,
+        maxBoundsViscosity: 0.9,
+        bounceAtZoomLimits: false,
+        tms: false,
+        noWrap: true,
+        opacity: 1.0,
+    }).addTo(mymap);
+
+mymap.setMaxBounds([
+    [11.475132, 104.8365973],
+    [11.622882, 105.020376]
+]);
+
+const markerData = {
+    casa: {
+        icon: {
+            className: 'custom-marker-right-up casa',
+            html: '<h5>FLATIRON BY MERIDIAN</h5>',
+            title: "flatiron"
+        },
+        position: [11.5726359, 104.9077589]
+    }
+};
+
+for (const markerDataKey in markerData) {
+    const marker = L.marker(markerData[markerDataKey].position, {
+        icon: L.divIcon(markerData[markerDataKey].icon),
+        zIndexOffset: 10000
+    }).addTo(mymap);
+    marker.on('click', function(){
+        window.location = "/map/index.html?mapMarker=flatiron";
+    })
+}
